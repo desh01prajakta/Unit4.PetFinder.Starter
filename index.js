960cf41c926419fc4049e0ec785f9e539eb54c80
desh01prajakta <prajudeshmukh11@gmail.com>
2024-01-22 21:58:59 -0600

    adds value

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // import the pets array from data.js
 const pets = require('./data');
+const path = require("path");
 
 // init express app
 const express = require('express');
@@ -9,6 +10,7 @@ const PORT = 8080;
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
     // serve up the public folder as static index.html file
 
 });
@@ -20,12 +22,15 @@ app.get('/api', (req, res) => {
 
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
+    const pets = req.query.name
+    res.send(pets)
     // send the pets array as a response
 
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
+    const owner = req.query.type
     // get the owner from the request
 
 
@@ -33,23 +38,25 @@ app.get('/api/v1/pets/owner', (req, res) => {
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
+    res.send(pet)
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
+    const name = req.params.name;
     // get the name from the request
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
-
-    // send the pet as a response
+        // send the pet as a response
+    res.send(pet)
 
 });
 
 app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
+    console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
